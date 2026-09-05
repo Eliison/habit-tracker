41a86eddcffc78e543f12be7baff9cc0e90ef1d7
Eliison <elisonalvesmonteiro9807@gmail.com>
2026-09-05 16:26:57 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,18 @@
-<h1>
-    Welcome to the Home Page
-</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
 
-<p>Hello, {{ $name }}!</p>
+    @vite(['resources/css/app.css'])
+</head>
 
-<ul>
-    @foreach ($habits as $habit)
-        <li>{{ $habit }}</li>
-    @endforeach
-</ul>
+<body>
+
+    <h1 class='text-3x font-bold'>
+        Hello world!
+    </h1>
+
+</body>
+</html>
