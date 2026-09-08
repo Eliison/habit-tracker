549f5d9a9d6cbb45c5ca0e73ac09dae613a9e10c
Eliison <elisonalvesmonteiro9807@gmail.com>
2026-09-08 17:44:41 -0300

    feat: init components header/footer/layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name') }}</title>
+<x-layout>
+    
+    <main class="py-10">
+        <h1>
+            Veja seus hábitos ganharem vida
+        </h1>
+    </main>
 
-    @vite(['resources/css/app.css'])
-</head>
-
-<body>
-
-    <h1 class='text-3x font-bold'>
-        Hello world!
-    </h1>
-
-</body>
-</html>
+</x-layout>
