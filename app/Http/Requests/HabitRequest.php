9823194874242e0f98012d9ce6136f9568558e2b
Eliison <elisonalvesmonteiro9807@gmail.com>
2026-09-15 22:25:11 -0300

    feat(habit): create new habit

---
+++ app/Http/Requests/HabitRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class HabitRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório.',
+            'name.string' => 'O campo nome deve ser texto.',
+            'name.max' => 'O campo nome não pode ter mais de 255 caracteres.'
+        ];
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     
     <main class="py-10">
-        <section class=" bg-white max-w-[600px] mx-auto p-6 border-2 mt-4">
+        <section class=" bg-white max-w-[600px] mx-auto p-10 pb-6 border-2 mt-4">
 
             <h1 class="font-bold text-3xl mb-4">
                 Faça login
